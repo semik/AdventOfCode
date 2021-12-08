2b3e336836c6147a14f189bb4f01c8e2804ea4f7
Jan Tomasek <jan@tomasek.cz>
2021-12-08 07:29:49 +0100

    2

--- 08/2.pl
+++ 08/2.pl
@@ -3,25 +3,35 @@
 use strict;
 use Data::Dumper;
 
-my $segments = {
-    2 => 1,
-    4 => 4,
-    3 => 7,
-    7 => 8,
+sub sortOutput {
+  my $a = shift;
+
+  return join('', sort split(//, $a));
 };
+
+my $segments = {
+		2 => 1,
+		4 => 4,
+		3 => 7,
+		7 => 8,
+	       };
+
+
 
 my $known = 0;
 while (my $line = <>) {
   chomp($line);
-  $line =~ s/^.*\| //;
-
+warn $line;
+  my %digits;
+  # identifikovat cisla ktery jsou jasny
   my @output = split(/ /, $line);
-  foreach my $output (@output) {
+  foreach my $output (map { sortOutput($_) } @output) {
     if (exists $segments->{length($output)}) {
       $known++;
       print "$output\n";
     };
   };
+  print "--\n";
 };
 
-print "$known\n";
+#print "$known\n";
