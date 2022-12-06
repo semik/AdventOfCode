2260be9e47a0050f9a07329c4e46e7462799cb79
Jan Tomasek <jan@tomasek.cz>
2022-12-06 15:04:27 +0100

    reseni 6. dne

---
+++ 2022/06/b.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Data::Dumper;
+use List::MoreUtils qw(uniq);
+
+my $line = <>; chomp($line);
+my @input = split(//, $line);
+
+my $counter = 0;
+my @buf = ();
+do {
+    my $c = shift @input;
+    push @buf, $c;
+    $counter++;
+
+    if (@buf == 14) {
+	if ((uniq @buf) == 14) {
+	    printf "first marker after %d characters: %s (%s)\n",
+		$counter, join('', @buf), $c;
+	    exit 0;
+	} else {
+	    shift @buf;
+	};
+    };
+} while @input;
+
+die Dumper(\@input);
